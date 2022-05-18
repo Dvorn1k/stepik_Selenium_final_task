fdd5e772c1690e51bdfc0bb6aa264b326c802ace
Vladislav <vladsermihailenko@gmail.com>
2022-05-18 16:14:00 +0200

    added new test cases and new method in Base Page

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 
 class LoginPageLocators:
@@ -10,9 +11,10 @@ class LoginPageLocators:
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 
-class ProductPageLocator:
+class ProductPageLocators:
     BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
     TITLE_OF_BOOK = (By.CSS_SELECTOR, ".product_main>h1")
     PRICE_OF_BOOK = (By.CSS_SELECTOR, ".product_main>.price_color")
     TITLE_IN_SUCCESS_TEXT = (By.CSS_SELECTOR, "#messages>:first-child>div>strong")
     PRICE_OF_BASKET = (By.CSS_SELECTOR, "#messages>:last-child>div>p>strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages>:first-child")
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,21 +1,30 @@
 from .base_page import BasePage
-from .locators import ProductPageLocator
-
+from .locators import ProductPageLocators
+import time
 
 class ProductPage(BasePage):
     def should_be_add_to_basket(self):
-        button = self.browser.find_element(*ProductPageLocator.BUTTON_ADD_TO_BASKET)
+        button = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
         button.click()
         BasePage.solve_quiz_and_get_code(self)
         self.should_be_added_correct_title_of_book_in_basket()
         self.should_be_added_correct_price_of_book_in_basket()
+        time.sleep(1)
 
     def should_be_added_correct_title_of_book_in_basket(self):
-        book_title = self.browser.find_element(*ProductPageLocator.TITLE_OF_BOOK)
-        basket_title = self.browser.find_element(*ProductPageLocator.TITLE_IN_SUCCESS_TEXT)
+        book_title = self.browser.find_element(*ProductPageLocators.TITLE_OF_BOOK)
+        basket_title = self.browser.find_element(*ProductPageLocators.TITLE_IN_SUCCESS_TEXT)
         assert book_title.text == basket_title.text, "The title of the book and the title in the basket do not match"
 
     def should_be_added_correct_price_of_book_in_basket(self):
-        book_price = self.browser.find_element(*ProductPageLocator.PRICE_OF_BOOK)
-        basket_price = self.browser.find_element(*ProductPageLocator.PRICE_OF_BASKET)
+        book_price = self.browser.find_element(*ProductPageLocators.PRICE_OF_BOOK)
+        basket_price = self.browser.find_element(*ProductPageLocators.PRICE_OF_BASKET)
         assert book_price.text == basket_price.text, "The price of the book and the price of the basket do not match"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(
+            *ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+
+    def should_success_message_is_disappeared(self):
+        assert self.is_disappeared(
+            *ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be 2"
