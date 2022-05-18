45eb7434acee7fefc7995fda9691252eb04decf7
Vladislav <vladsermihailenko@gmail.com>
2022-05-18 18:14:21 +0200

    added test cases for loggin in users

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
-
+import faker
+import time
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -16,3 +17,17 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self):
+        f = faker.Faker()
+        email = f.email()
+        input1 = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_EMAIL)
+        input1.send_keys(email)
+        password = 9078563412
+        input2 = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_PASSWORD)
+        input2.send_keys(password)
+        input3 = self.browser.find_element(*LoginPageLocators.REGISTER_FORM_PASSWORD_REPEATING)
+        input3.send_keys(password)
+        button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        button.click()
+        time.sleep(5)
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,30 @@
+from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 import pytest
+
+@pytest.mark.login
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user()
+        page.should_be_authorized_user()
+
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_add_to_basket()
 
 
 @pytest.mark.parametrize('promo_offer',
